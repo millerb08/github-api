0ab7577f4f885a20746ad436d00e775349e9576b
millerb08 <miller.steffens@gmail.com>
2020-04-30 16:18:55 -0500

    curl.php file

--- curl.php
+++ curl.php
@@ -1,5 +1,5 @@
 <?php
-$username = 'mikethefrog'; //step2
+/*$username = 'mikethefrog'; //step2
 //+authentication token
 $url = 'https://api.github.com/users/treehouse';
 $process = curl_init($url); 
@@ -14,4 +14,15 @@ if (json_last_error() === JSON_ERROR_NONE) {
     var_dump($json);
 } else {
 	var_dump($return);
-}
+}*/
+
+$username = "mikethefrog";
+$url = "https://api.github.com/users/treehouse";
+$process = curl_init($url);
+curl_setopt($process, CURLOPT_USERAGENT, $username);
+curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
+$return = curl_exec($process);
+$results = json_decode($return);
+//var_dump($results);
+echo "<img src='".$results->avatar_url."'/>";
+curl_close($process);
