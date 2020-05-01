73b13750345992604110ea965a45aa8c17c47b5c
millerb08 <miller.steffens@gmail.com>
2020-04-30 19:26:55 -0500

    fix uri and add search function

--- search.php
+++ search.php
@@ -3,5 +3,11 @@ require __DIR__ . '/vendor/autoload.php';
 
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader);
-
-echo $twig->render('search.html', ['title' => 'Search']);
+$api = new Milo\Github\Api;
+$args = ['title' => 'Search'];
+if(!empty($_GET["q"])){
+  $response = $api->get("/search/repositories/", ["q" => filter_input(INPUT_GET, "q", FILTER_SANITIZE_STRING)]);
+  $repositories = $api->decode($response);
+  $args["items"] = $repositories->items;
+}
+echo $twig->render('search.html', $args);
