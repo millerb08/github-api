016a15f102f6f0191e31b0b017ad7e84a9f86f4b
millerb08 <miller.steffens@gmail.com>
2020-04-30 20:41:50 -0500

    add subscription

---
+++ inc/watch.php
@@ -0,0 +1,20 @@
+<?php
+
+if(isset($_GET["subscribe"])){
+  $response = $api->put("/repos/".$_GET["subscribe"]."/subscription/",["subscribed" => true]);
+}
+
+if(isset($_GET["unsubscribe"])){
+  $response = $api->delete("/repos/".$_GET["unsubscribe"]."/subscription/",["subscribed" => false]);
+}
+
+$alert = array();
+if(isset($response)){
+  if(substr($response->getCode(),0,1) == 2){
+    $alert["type"] = "success";
+    $alert["title"] = "Watch Status Updated";
+  }else{
+    $alert["type"] = "danger";
+    $alert["title"] = "Unable to Update Watch Status";
+  }
+}
